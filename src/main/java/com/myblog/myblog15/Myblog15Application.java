aaf8ff79f514a2818dc5d8f1827768138cf44757
Lenovo@UJWAL <ujwals1818@gmail.com.com>
2024-04-04 14:36:43 +0530

    127:Create save Post Feature

---
+++ src/main/java/com/myblog/myblog15/Myblog15Application.java
@@ -0,0 +1,13 @@
+package com.myblog.myblog15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myblog15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myblog15Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/myblog/myblog15/Myblog15ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myblog.myblog15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myblog15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
